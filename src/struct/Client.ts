e108bbbf88b6e1f87465c592b6eb966b0dd6856d
Venny <sgtmattis@gmail.com>
2020-07-15 13:57:38 +0200

    Base structure

--- src/struct/Client.ts
+++ src/struct/Client.ts
@@ -32,28 +32,28 @@ export class VenClient extends Client {
 			defaultImageSize: 512,
 			disableEvents: {
 				/* eslint-disable @typescript-eslint/naming-convention */
-				CHANNEL_CREATE: true, 		// channel created
-				CHANNEL_DELETE: true, 		// channel deleted
-				CHANNEL_UPDATE: true, 		// channel properties (name, topic, bitrate, etc.) were changed
-				GUILD_BAN_ADD: true, 		// user was banned
-				GUILD_BAN_REMOVE: true, 	// user was unbanned
-				GUILD_CREATE: false, 		// bot joined a guild
-				GUILD_DELETE: false, 		// bot left a guild
-				GUILD_MEMBER_ADD: true, 	// user joined a guild
-				GUILD_MEMBER_REMOVE: true, 	// user left a guild
-				GUILD_MEMBER_UPDATE: true, 	// guild member properties (roles, nick) were changed
-				GUILD_ROLE_CREATE: true, 	// role was created
-				GUILD_ROLE_DELETE: true, 	// role was deleted
-				GUILD_ROLE_UPDATE: true, 	// role properties (name, permissions, color, etc.) were updated
-				GUILD_UPDATE: true, 		// guild properties (name, icon, region, etc.) were updated
-				MESSAGE_CREATE: false, 		// message was created
-				MESSAGE_DELETE: true, 		// message was deleted
-				MESSAGE_DELETE_BULK: true, 	// many messages were deleted at once
-				MESSAGE_UPDATE: false, 		// message properties (content, editedTimestamp, embeds, etc.) were changed
-				PRESENCE_UPDATE: true, 		// user presence (status, game) was changed, or user was updated (avatar, discriminator, username)
-				TYPING_START: true, 		// user started typing
-				USER_UPDATE: false, 		// bot user was updated
-				VOICE_STATE_UPDATE: false 	// user voice state (current voice channel, mute, selfDeaf, etc.) was changed
+				CHANNEL_CREATE: true, // channel created
+				CHANNEL_DELETE: true, // channel deleted
+				CHANNEL_UPDATE: true, // channel properties (name, topic, bitrate, etc.) were changed
+				GUILD_BAN_ADD: true, // user was banned
+				GUILD_BAN_REMOVE: true, // user was unbanned
+				GUILD_CREATE: false, // bot joined a guild
+				GUILD_DELETE: false, // bot left a guild
+				GUILD_MEMBER_ADD: true, // user joined a guild
+				GUILD_MEMBER_REMOVE: true, // user left a guild
+				GUILD_MEMBER_UPDATE: true, // guild member properties (roles, nick) were changed
+				GUILD_ROLE_CREATE: true, // role was created
+				GUILD_ROLE_DELETE: true, // role was deleted
+				GUILD_ROLE_UPDATE: true, // role properties (name, permissions, color, etc.) were updated
+				GUILD_UPDATE: true, // guild properties (name, icon, region, etc.) were updated
+				MESSAGE_CREATE: false, // message was created
+				MESSAGE_DELETE: true, // message was deleted
+				MESSAGE_DELETE_BULK: true, // many messages were deleted at once
+				MESSAGE_UPDATE: false, // message properties (content, editedTimestamp, embeds, etc.) were changed
+				PRESENCE_UPDATE: true, // user presence (status, game) was changed, or user was updated (avatar, discriminator, username)
+				TYPING_START: true, // user started typing
+				USER_UPDATE: false, // bot user was updated
+				VOICE_STATE_UPDATE: false // user voice state (current voice channel, mute, selfDeaf, etc.) was changed
 				/* eslint-enable @typescript-eslint/naming-convention */
 			},
 			compress: true
